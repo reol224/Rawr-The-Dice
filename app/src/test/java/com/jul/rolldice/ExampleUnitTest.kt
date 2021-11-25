f792a2e13996e3d2a2be5da72887f39178998afb
Iulia Danilov <tmdoppelganger@gmail.com>
2021-11-25 15:15:03 -0500

    optimized imports

--- app/src/test/java/com/jul/rolldice/ExampleUnitTest.kt
+++ app/src/test/java/com/jul/rolldice/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.jul.rolldice
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
